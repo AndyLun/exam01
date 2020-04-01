a501123b3b9ee23dab4b1ecd5e54bdec58e5bfc8
AndyLun <andylun2009@gmail.com>
2020-04-01 17:02:04 +0800

    PWM first test.

--- Q2_uLCD/main.cpp
+++ Q2_uLCD/main.cpp
@@ -2,6 +2,10 @@
 #include "uLCD_4DGL.h"
 
 uLCD_4DGL uLCD(D1, D0, D2);
+PwmOut pwm(D6);
+Timer timSweep;
+float duty = 0.0;
+int increasing = 1;
 
 int main() {
 	uLCD.printf("ID: 107000270");
@@ -9,5 +13,24 @@ int main() {
 	uLCD.line(100, 25, 100, 100, WHITE);
 	uLCD.line(25, 25, 25, 100, WHITE);
 	uLCD.line(25, 100, 100, 100, WHITE);
-	wait(30);
+	
+	pwm.period(0.001);
+	timSweep.start();
+
+	while(true) {
+		pwm = duty;
+		if(timSweep > 0.1) {
+			timSweep.reset();
+
+			if(increasing) {
+				duty += 0.1;
+				if(duty == 1) increasing = 0;
+			} else {
+				duty -= 0.1;
+				if(duty == 0) increasing = 1;
+			}
+		}
+
+		
+	}
 }
