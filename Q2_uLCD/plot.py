28b872c35d1e739f7a514d6daf7b4e07fdeb07fa
AndyLun <andylun2009@gmail.com>
2020-04-01 17:27:49 +0800

    Added python code.

---
+++ Q2_uLCD/plot.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import serial
+import time
+
+t = np.arange(0, 12.8, 0.1)
+y = np.arange(0, 12.8, 0.1)
+
+serdev = '/dev/ttyACM0'
+s = serial.Serial(serdev)
+
+for x in range(0, 128):
+	line=s.readline()
+	print(line)
+	y[x] = int(line)
+
+plt.plot(t,y)
+#plt.set_xlabel('Time')
+#plt.set_ylabel('Amplitude')
+plt.show()
+s.close()
