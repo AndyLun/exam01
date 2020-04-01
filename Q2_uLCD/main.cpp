5bbd23c2780c814041215b4a62ecaff84949e155
AndyLun <andylun2009@gmail.com>
2020-04-01 16:48:53 +0800

    Square drawn.

--- Q2_uLCD/main.cpp
+++ Q2_uLCD/main.cpp
@@ -5,6 +5,9 @@ uLCD_4DGL uLCD(D1, D0, D2);
 
 int main() {
 	uLCD.printf("ID: 107000270");
-	uLCD.line(5, 5, 10, 5, WHITE);
+	uLCD.line(15, 15, 60, 15, WHITE);
+	uLCD.line(60, 15, 60, 60, WHITE);
+	uLCD.line(15, 15, 15, 60, WHITE);
+	uLCD.line(15, 60, 60, 60, WHITE);
 	wait(30);
 }
